930e1c59162418b33895e5936900fbd688ba3a1d
jay51 <nenojaafar3@gmail.com>
2018-10-05 18:58:10 -0500

    modified:   index.js added 404 middelware and costume error 	handler 	modified:   package.json installed morgan logger

--- index.js
+++ index.js
@@ -4,24 +4,33 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const routes = require("./routes");
+const logger = require("morgan");
 
-const checkJson = function(req, res, next) {
-	if (req.body) {
-		console.log("The sky is", req.body.color);
-	} else {
-		console.log("There's no body property");
-	}
-	next();
-};
-// befor using body parser the req.body don't exist
-app.use(checkJson);
-// body parser middel for json parser
-app.use(bodyParser());
-// now req.body exist
-app.use(checkJson);
+app.use(logger("dev"));
+app.use(bodyParser.json());
 
 // questions routes
 app.use("/questions/", routes);
+
+// when a route throws an error, it will contunie going down to middelwares
+// and this middleware will send 404 errors to error handler. without this middleware
+// the browser will return default error handler
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	const err = new Error("Not Found");
+	err.status = 404;
+	next(err);
+});
+
+// Error handler
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+	res.json({
+		error: {
+			message: err.message
+		}
+	});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function(err) {
--- routes.js
+++ routes.js
@@ -5,6 +5,7 @@ const router = require("express").Router();
 // GET /questions
 router.get("/", function(req, res) {
 	// return all questions
+	// const o = obj.prop; // this will throw error status code 500
 	res.json({
 		response: "You sent me a GET request"
 	});
@@ -12,19 +13,73 @@ router.get("/", function(req, res) {
 
 // POST /questions
 router.post("/", function(req, res) {
-	// return all questions and body
+	// create  question
 	res.json({
 		response: "You sent me a POST request",
 		body: req.body
 	});
 });
 
-// GET /questions/:id
-router.get("/:id", function(req, res) {
-	// return questions with id
+// GET /questions/:qID
+router.get("/:qID", function(req, res) {
+	// return questions with qID
 	res.json({
-		response: "You sent me a GET request for ID " + req.params.id
+		response: "You sent me a GET request for qID " + req.params.qID
 	});
 });
+
+// POST /questions/:qID/answers
+router.post("/:qID/answers", function(req, res) {
+	// create new answer
+	res.json({
+		response: "You sent me a POST request for new answer",
+		questionId: req.params.qID,
+		body: req.body
+	});
+});
+
+// PUT /questions/:qID/answers/aID
+router.put("/:qID/answers/:aID", function(req, res) {
+	// edit  answer
+	res.json({
+		response: "You sent me a put request for edit answer",
+		questionId: req.params.qID,
+		answerId: req.params.aID,
+		body: req.body
+	});
+});
+
+// DELETE /questions/:qID/answers/aID
+router.delete("/:qID/answers/:aID", function(req, res) {
+	// delete  answer
+	res.json({
+		response: "You sent me a delete request  answer",
+		questionId: req.params.qID,
+		answerId: req.params.aID
+	});
+});
+
+// post /questions/:qID/answers/vote-:dir
+router.post(
+	"/:qID/answers/:aID/vote-:dir",
+	function(req, res, next) {
+		if (req.params.dir.search(/^(up|down)$/) === -1) {
+			const err = new Error("Not Foound");
+			err.status = 404;
+			next(err);
+		} else {
+			next();
+		}
+	},
+	function(req, res) {
+		// post  vote
+		res.json({
+			response: "You sent me a POST request for edit answer",
+			questionId: req.params.qID,
+			answerId: req.params.aID,
+			vote: req.params.dir
+		});
+	}
+);
 
 module.exports = router;
