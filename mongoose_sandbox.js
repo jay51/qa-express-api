b57af32623bbc1eaf05a411cf69f06f53e0baa13
jay51 <nenojaafar3@gmail.com>
2018-10-08 00:12:51 -0500

    added more examples on using mongoose & Animal.find() returns undefined

--- mongoose_sandbox.js
+++ mongoose_sandbox.js
@@ -18,29 +18,60 @@ db.once("open", function() {
 
 	const Schema = mongoose.Schema;
 	const AnimalSchema = new Schema({
-		type: String,
-		size: String,
-		color: String,
-		mass: Number,
-		name: String
+		type: { type: String, default: "goldfish" },
+		size: { type: String, default: "small" },
+		color: { type: String, default: "orange" },
+		mass: { type: Number, default: "10" },
+		name: { type: String, default: "nick" }
 	});
 
 	// create mongoose model
 	const Animal = mongoose.model("Animal", AnimalSchema);
 
-	const cat = new Animal({
-		type: "cat",
-		size: "small",
-		color: "blue",
-		mass: 50,
-		name: "muphin"
-	});
+	// remove everything in the Animal model
+	Animal.deleteMany({}, function(err) {
+		if (err) console.log(err);
 
-	cat.save(function(err) {
-		if (err) console.log("save Failed", err);
-		else console.log("saved!");
-		db.close(function() {
-			console.log("db closed!");
+		const cat = new Animal({
+			type: "cat",
+			size: "small",
+			color: "blue",
+			mass: 50,
+			name: "muphin"
+		});
+
+		const anotherAnimal = new Animal({}); // goldfish
+
+		const whale = new Animal({
+			type: "whale",
+			size: "big",
+			mass: 3444,
+			name: "fig"
+		});
+
+		cat.save(function(err) {
+			if (err) console.log("save Failed", err);
+			else console.log("saved!");
+
+			anotherAnimal.save(function(err) {
+				if (err) console.log("save Failed", err);
+
+				whale.save(function(err) {
+					if (err) console.log("save Failed", err);
+
+					// find animals
+					Animal.find({}, function(err, animals) {
+						console.log(animals);
+						// animals.forEach(function(anim) {
+						// 	console.log(`${anim.name} the ${anim.color} ${anim.type}`);
+						// });
+					});
+
+					db.close(function() {
+						console.log("db closed!");
+					});
+				});
+			});
 		});
 	});
 });
