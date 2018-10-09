ee063914161de79a6ea5e660df8750228235058c
jay51 <nenojaafar3@gmail.com>
2018-10-09 01:44:49 -0500

    added more examples on how to use mongoose

--- mongoose_sandbox.js
+++ mongoose_sandbox.js
@@ -19,58 +19,113 @@ db.once("open", function() {
 	const Schema = mongoose.Schema;
 	const AnimalSchema = new Schema({
 		type: { type: String, default: "goldfish" },
-		size: { type: String, default: "small" },
+		size: String,
 		color: { type: String, default: "orange" },
 		mass: { type: Number, default: "10" },
 		name: { type: String, default: "nick" }
 	});
 
+	// pre "save" hocks let you do something to the document before it's saved
+	AnimalSchema.pre("save", function(next) {
+		if (this.mass >= 100) {
+			this.size = "big";
+		} else if (this.mass >= 5) {
+			this.size = "medium";
+		} else {
+			this.size = "smalll";
+		}
+		next();
+	});
+
+	//static methods are a coustme method we can call on the model
+	//costume find query to find animals by size
+	AnimalSchema.statics.findBySize = function(size, callback) {
+		// this == Animal
+		return this.find({ size }, callback);
+		// callback will be passed the find result
+	};
+
+	// instance methods are custome methods we can call on the instance
+	// instance method to find all animals with the same color as the instance
+	AnimalSchema.methods.findBySameColor = function(callback) {
+		// this == instance/document
+		// search the animal model for animals with same color as current animal
+		return this.model("Animal").find({ color: this.color }, callback);
+	};
+
 	// create mongoose model
 	const Animal = mongoose.model("Animal", AnimalSchema);
 
-	// remove everything in the Animal model
-	Animal.deleteMany({}, function(err) {
-		if (err) console.log(err);
+	const cat = new Animal({
+		type: "cat",
+		size: "small",
+		color: "blue",
+		mass: 50,
+		name: "muphin"
+	});
 
-		const cat = new Animal({
-			type: "cat",
-			size: "small",
-			color: "blue",
-			mass: 50,
-			name: "muphin"
-		});
+	const whale = new Animal({
+		type: "whale",
+		size: "big",
+		mass: 3444,
+		name: "fig"
+	});
 
-		const anotherAnimal = new Animal({}); // goldfish
+	const animalData = [
+		{
+			type: "mouse",
+			color: "gray",
+			mass: 3.2,
+			name: "marvin"
+		},
+		{
+			type: "bird",
+			color: "white",
+			mass: 6.4,
+			name: "slick"
+		},
+		{
+			type: "wolf",
+			color: "black",
+			mass: 45.4,
+			name: "Iron"
+		},
+		cat,
+		whale
+	];
 
-		const whale = new Animal({
-			type: "whale",
-			size: "big",
-			mass: 3444,
-			name: "fig"
-		});
+	Animal.remove({}, function(err) {
+		if (err) console.error("some err", err);
+		Animal.create(animalData, function(err, animals) {
+			if (err) console.error("error when creating new instance");
 
-		cat.save(function(err) {
-			if (err) console.log("save Failed", err);
-			else console.log("saved!");
+			animals.forEach(function(animal) {
+				console.log(
+					`${animal.name} the ${animal.color} ${animal.type} is a ${
+						animal.size
+					}`
+				);
+			});
 
-			anotherAnimal.save(function(err) {
-				if (err) console.log("save Failed", err);
+			Animal.find({}, function(err, all) {
+				console.log(err);
+			});
+			// Animal.findBySize("small", function(err, animal) {
+			// 	console.log(
+			// 		`${animal.name} the ${animal.color} ${animal.type} is a ${
+			// 			animal.size
+			// 		}`
+			// 	);
+			// });
 
-				whale.save(function(err) {
-					if (err) console.log("save Failed", err);
+			// Animal.findOne({ type: "cat" }, function(err, cat) {
+			// 	cat.findBySameColor(function(err, animals_with_same_color_as_cat) {
+			// 		console.log(animals_with_same_color_as_cat);
+			// 	});
+			// });
 
-					// find animals
-					Animal.find({}, function(err, animals) {
-						console.log(animals);
-						// animals.forEach(function(anim) {
-						// 	console.log(`${anim.name} the ${anim.color} ${anim.type}`);
-						// });
-					});
-
-					db.close(function() {
-						console.log("db closed!");
-					});
-				});
+			db.close(function() {
+				console.log("db connection closed");
 			});
 		});
 	});
