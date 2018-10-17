480e805d98101f5ef6634050ee7ce179a1332a41
jay51 <nenojaafar3@gmail.com>
2018-10-16 23:51:30 -0500

    modified:   index.js add middelware for cors

--- index.js
+++ index.js
@@ -26,11 +26,31 @@ db.once("open", function() {
 	console.log("db connection successful");
 });
 
+// middelware for cros
+app.use(function(req, res, next) {
+	// modififes the header on the response to let the broswer know what it can and can't do.
+	// restricts the api to the domains which are allowed to access the api. * means everyone allowed
+	res.header("Access-Control-Allow-Origin", "*");
+	// tells the browser which headers are permited/allowed
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	// check for a pref-light request, pref-light requests come with req.method=Options
+	if (req.method === "OPTIONS") {
+		// req.method=options isn't handeld by any of our routes, so let's handle it here!
+		// return response with header to let the browser know the methods allowed in this api
+		res.header("Access-Control-Allow-Method", "GET POST PUT DELETE");
+		return res.status(200).json({});
+	}
+	next();
+});
+
 // questions routes
 app.use("/questions/", routes);
 
 // when no route matches the route requsted
-//  this middleware will send 404 errors to error handler. without this middleware
+// this middleware will send 404 errors to error handler. without this middleware
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
 	const err = new Error("Not Found");
